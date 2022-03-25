0d0a3a6e152613a1c2c1555fabb1aed0656422cf
McTurner45 <58169969+nmunatsi@users.noreply.github.com>
2022-03-25 09:47:32 +0200

    validations

--- esr21_pharmacy/forms/drug_accountability_log_form.py
+++ esr21_pharmacy/forms/drug_accountability_log_form.py
@@ -1,13 +1,10 @@
 from django import forms
 from edc_form_validators import FormValidatorMixin
 
-from esr21_pharmacy_validations.form_validations import \
-    DrugAccountabilityLogFormValidator
 from ..models.drug_accountability_log import DrugAccountabilityLog
 
 
 class DrugAccountabilityLogForm(FormValidatorMixin, forms.ModelForm):
-    form_validator_cls = DrugAccountabilityLogFormValidator
 
     class Meta:
         models = DrugAccountabilityLog
